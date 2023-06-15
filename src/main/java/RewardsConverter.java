9717c941cdafd05f8fe6e30ea915297578b1f109
Jatin <jatin011101@gmail.com>
2023-06-16 00:09:51 +0530

    Updated RewardConverter and created RewardValue class

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class RewardsConverter {
     public static void main(String[] args) {
+
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
@@ -16,5 +17,7 @@ public class RewardsConverter {
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+
+
     }
 }
